c1fbc6360233d0db86b76bb1501239f7fc7b0477
Michele Sacchetti <michele.sacchetti@gmail.com>
2015-12-12 00:59:42 +0100

    maven plugin skeleton

---
+++ pathfinder-maven-plugin/src/main/java/org/aroundthecode/pathfinder/maven/plugin/treeSerializers/LogNodeVisitor.java
@@ -0,0 +1,88 @@
+package org.aroundthecode.pathfinder.maven.plugin.treeSerializers;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.maven.plugin.dependency.tree.AbstractSerializingVisitor;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
+
+public class LogNodeVisitor extends AbstractSerializingVisitor implements
+		DependencyNodeVisitor {
+
+	private Log log;
+	private String prj = null;
+
+	public LogNodeVisitor(Writer writer, Log log) {
+		super(writer);
+		this.log = log;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean visit(DependencyNode node) {
+
+		
+		log.info("visiting "+node.toNodeString());
+		
+		if (node.getParent() == null || node.getParent() == node) {
+			prj =  node.toNodeString();
+			writer.write("Project is:[" + prj + "]\n");
+			//STORE PRJ TO DB			
+			
+		}
+
+		// Generate "currentNode -> Child" lines
+
+		List<DependencyNode> children = node.getChildren();
+		boolean out = true;
+		for (Iterator<DependencyNode> it = children.iterator(); it.hasNext();) {
+			String szFrom 	= node.toNodeString();
+			String szTo 	= it.next().toNodeString();
+
+			StringBuffer sb = new StringBuffer();
+			sb.append(szFrom);
+			sb.append(" requires ");
+			sb.append(szTo);
+			sb.append("\n");
+			writer.println(sb.toString());
+			
+			//STORE SZFROM TO DB
+			//STORE SZTO TO DB
+			//STORE PRJ-FROM-TO TO DB
+			
+		}
+
+		return out;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean endVisit(DependencyNode node) {
+		return true;
+	}
+
+}
--- pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/Application.java
+++ pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/Application.java
@@ -42,14 +42,12 @@ public class Application extends Neo4jConfiguration implements CommandLineRunner
 		}
 	}
 
-	@Autowired ArtifactRepository artifactRepository;
+//	@Autowired ArtifactRepository artifactRepository;
 
-	@Autowired GraphDatabase graphDatabase;
+//	@Autowired GraphDatabase graphDatabase;
 
 	@Autowired
 	GraphDatabaseService db;
-
-	@Autowired Neo4jOperations template;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,90 +67,6 @@ public class Application extends Neo4jConfiguration implements CommandLineRunner
 			e.printStackTrace();
 		}
 		// end of Neo4j browser config
-
-
-		String uniqueIdA1 = "group1:artifact1:version1:jar:";
-		String uniqueIdA2 = "group2:artifact2:version2:war:";
-		String uniqueIdA3 = "group3:artifact3:version3:war:altro";
-
-		Artifact a1 = new Artifact();
-		a1.setUniqueId(uniqueIdA1);
-		Artifact a2 = new Artifact();
-		a2.setUniqueId(uniqueIdA2);
-		Artifact a3 = new Artifact();
-		a3.setUniqueId(uniqueIdA3);
-
-		System.out.println("Before linking up with Neo4j...");
-		for (Artifact a : new Artifact[] { a1, a2, a3 }) {
-			System.out.println(a);
-		}
-
-		Transaction tx = graphDatabase.beginTx();
-		try {
-			artifactRepository.save(a1);
-			artifactRepository.save(a2);
-			artifactRepository.save(a3);
-
-			for (Iterator<Artifact> i = artifactRepository.findAll().iterator(); i.hasNext();) {
-				Artifact a = i.next();
-				if(a!=null){ 
-					System.out.println("FOUND");
-				}
-				if(a.getUniqueId()!=null){
-					System.out.println(a);
-				}
-				else{
-					System.out.println("but empty!");
-				}
-
-			}
-
-			a1 = artifactRepository.findByUniqueId(uniqueIdA1);
-			if(a1!=null){ 
-				System.out.println("A1 FOUND");
-			}
-			if(a1.getUniqueId()!=null){
-				System.out.println(a1);
-			}
-			else{
-				System.out.println("but empty!");
-			}
-			a1.dependsOn(a2);
-			a1.dependsOn(a3);
-			artifactRepository.save(a1);
-
-			a2 = artifactRepository.findByUniqueId(uniqueIdA2);
-			a2.dependsOn(a3);
-			artifactRepository.save(a2);
-//
-			/*
-//
-			for (Iterator<Artifact> i = artifactRepository.findAll().iterator(); i.hasNext();) {
-				Artifact a = i.next();
-				if(a!=null){ 
-					System.out.println("FOUND");
-				}
-				if(a.getUniqueId()!=null){
-				System.out.println(a);
-				}
-				else{
-					System.out.println("but empty!");
-				}
-
-			}
-//			
-			 */
-
-			//			System.out.println("Looking up who depends on art1...");
-			//			for (MavenArtifact a : mavenArtifactRepository.findByDependencyArtifact(art1)) {
-			//				System.out.println(a.getArtifact() + " depends on art1.");
-			//			}
-			tx.success();
-		} finally {
-			tx.close();
-		}
-
-
 
 	}
 
