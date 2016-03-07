6681eba8cd2495e3c585172d7aa403d60d07bd82
Michele Sacchetti <michele.sacchetti@aroundthecode.org>
2016-03-07 23:44:19 +0100

    M2_HOME detection for travis build

--- pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/crawler/CrawlerWrapper.java
+++ pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/crawler/CrawlerWrapper.java
@@ -11,15 +11,32 @@ import org.apache.maven.shared.invoker.MavenInvocationException;
 
 public class CrawlerWrapper {
 
+	private static String mvnHome = "/opt/apache-maven";
+	
 	static void crawl(String groupId,String artifactId,String packaginf,String classifier, String version) throws MavenInvocationException{
 		
 		InvocationRequest request = new DefaultInvocationRequest();
 		request.setPomFile( new File( "./src/main/resources/embedder/pom.xml" ) );
 		request.setGoals(  Collections.singletonList( "org.aroundthecode.pathfinder:pathfinder-maven-plugin:0.1.0-SNAPSHOT:crawler" ) );
 		Invoker invoker = new DefaultInvoker();
-		invoker.setMavenHome( new File("/opt/apache-maven") );
+		
+		String mHome = System.getenv("M2_HOME");
+		if(mHome==null){
+			System.err.println("No M2_HOME set, using ["+getMvnHome()+"]");
+			mHome = getMvnHome();
+		}
+		
+		invoker.setMavenHome( new File(mHome) );
 		invoker.execute( request );
 		
+	}
+
+	public static String getMvnHome() {
+		return mvnHome;
+	}
+
+	public void setMvnHome(String mvnHome) {
+		this.mvnHome = mvnHome;
 	}
 	
 	
