60f315cc74dd0b7469012060223db9f0abac7b09
Michele Sacchetti <michele.sacchetti@aroundthecode.org>
2016-03-07 23:27:44 +0100

    Cleanup, keeping only maven-invoker for already existing pathfinder plugin

--- pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/crawler/CrawlerWrapper.java
+++ pathfinder-server/src/main/java/org/aroundthecode/pathfinder/server/crawler/CrawlerWrapper.java
@@ -11,18 +11,7 @@ import org.apache.maven.shared.invoker.MavenInvocationException;
 
 public class CrawlerWrapper {
 
-//	static MavenCli cli = new MavenCli();
-//	static String fd = new File(".").getAbsolutePath();
-//	static{
-//		fd += "/src/main/resources/embedder/";
-//		System.setProperty("maven.multiModuleProjectDirectory", fd);
-//	}
-	
 	static void crawl(String groupId,String artifactId,String packaginf,String classifier, String version) throws MavenInvocationException{
-		
-//		cli.doMain(
-//				new String[] {"-Xmx1G","-XX:MaxPermSize=512m", "org.aroundthecode.pathfinder:pathfinder-maven-plugin:0.1.0-SNAPSHOT:crawler" },
-//				fd, System.out, System.out);
 		
 		InvocationRequest request = new DefaultInvocationRequest();
 		request.setPomFile( new File( "./src/main/resources/embedder/pom.xml" ) );
