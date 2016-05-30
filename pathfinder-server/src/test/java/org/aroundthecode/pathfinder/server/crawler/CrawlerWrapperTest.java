6b6cf9c127561ab8d6266f6c3166c6a701a6c3d7
Viktor Sadovnikov <viktor@jv-ration.com>
2016-05-30 13:49:22 +0200

    Testing availability of Maven for CrawlerWrapper and dummy pom

---
+++ pathfinder-server/src/test/java/org/aroundthecode/pathfinder/server/crawler/CrawlerWrapperTest.java
@@ -0,0 +1,37 @@
+package org.aroundthecode.pathfinder.server.crawler;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+public class CrawlerWrapperTest {
+
+    /**
+     * This is a canary test of CrawlerWrapper setup.
+     * Fails if Maven installation is not found or there is a problem with dummy pom
+     */
+    @Test
+    public void testMavenInstallation() {
+
+        DefaultInvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(CrawlerWrapper.getFilePom());
+        request.setGoals(Collections.singletonList("clean"));
+
+        try {
+            InvocationResult result = CrawlerWrapper.getInvoker().execute(request);
+            assertNull("Invocation resulted in exception. " + result.getExecutionException().toString(), result.getExecutionException());
+            assertEquals("Maven invocation on dummy pom did not succeed", 0, result.getExitCode());
+        } catch (MavenInvocationException e) {
+            fail("CrawlerWrapper did not find Maven installation. " + e.toString());
+        }
+
+    }
+
+}
